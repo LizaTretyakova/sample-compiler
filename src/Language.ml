bac0d6cb51ec69d3227156a1b1471866f814df7b
LizaTretyakova <elizabet.tretyakova@gmail.com>
2016-11-25 12:56:59 +0300

    CHanged parsing of read

--- src/Language.ml
+++ src/Language.ml
@@ -115,8 +115,7 @@ module Stmt =
         };
 
         simple:
-          %"read"  "(" x:IDENT ")"         {Read x}
-        | %"write" "(" e:!(Expr.parse) ")" {Write e}
+          %"write" "(" e:!(Expr.parse) ")" {Write e}
         | %"skip"                          {Skip}
         | %"if" e:!(Expr.parse) %"then" s1:!(parse) %"else" s2:!(parse) %"fi" {If (e, s1, s2)}
         | %"if" e:!(Expr.parse) %"then" s1:!(parse) %"fi" {If (e, s1, Skip)}
@@ -124,6 +123,7 @@ module Stmt =
         | %"repeat" s:!(parse) %"until" e:!(Expr.parse) {Seq (s, While (condz, e, s))}
         | %"for" s1:!(parse) "," e:!(Expr.parse) "," s2:!(parse) %"do" s:!(parse) %"od" {Seq(s1, While (condnz, e, Seq (s, s2)))}
         | %"return" e:!(Expr.parse) {Return e}
+        | x:IDENT ":=" %"read" "()"        {Read x}
         | x:IDENT ":=" e:!(Expr.parse)     {Assign (x, e)}
         | f:IDENT "(" args:!(Util.list0 Expr.parse) ")" {Call (f, args)}
     )
